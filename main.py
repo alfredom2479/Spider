583e4ccf771768f84032cb1c4154bd7820e1263a
Alfredo <alfredomendez2479@gmail.com>
2023-09-24 15:35:47 -0400

    Make spider recursively crawl and print

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
-#https://en.wikipedia.org/wiki/Doughnut
+from urllib.parse import urljoin
 
-import requests;
+import requests
 from bs4 import BeautifulSoup
+from urllib import *
 
-def get_page(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content,  "html.parser")
+visited_urls = set()
 
-    tag = soup.find_all("a")
+def spider_urls(url, keyword):
+    try:
+        response = requests.get(url)
 
-    for t in tag:
-        url2 = t.get("href")
-        print(url2)
+    except:
+        print(f"Request failed {url}")
+        return
 
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
 
-get_page(input("Url to scrape: "))
+        a_tag = soup.find_all("a")
+        urls = []
+        for tag in a_tag:
+            href = tag.get("href")
+            if href is not None and href != "":
+                urls.append(href)
+        #print(urls)
+
+        for single_url in urls:
+            if single_url not in visited_urls:
+                visited_urls.add(single_url)
+                url_join = urljoin(url, single_url)
+                if keyword in url_join:
+                    print(url_join)
+                    spider_urls(url_join,keyword)
+            else:
+                pass
+
+url = input("URL to scrape: ")
+keyword = input("Keyword to search for: ")
+spider_urls(url, keyword)
+
+#https://www.yahoo.com
+
